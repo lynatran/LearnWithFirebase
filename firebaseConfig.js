518908885a22d5d48edda14b108fc905966821e8
Lyna Tran <lynatran8898@gmail.com>
2023-11-23 19:39:03 -0500

    Completed todo list!

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -12,13 +12,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.2/firebase
 
 // STEP 1: Declare Firebase Environment Variables  
 const firebaseConfig = {
-    apiKey: "YOUR-OWN-VARIABLE",
-    authDomain: "YOUR-OWN-VARIABLE",
-    projectId: "YOUR-OWN-VARIABLE",
-    storageBucket: "YOUR-OWN-VARIABLE",
-    messagingSenderId: "YOUR-OWN-VARIABLE",
-    appId: "YOUR-OWN-VARIABLE",
-};
+    apiKey: "AIzaSyCYfQSRNPWDHG66Pt_tGLnXnTJXhp6LKo4",
+    authDomain: "learn-with-firebase-653a1.firebaseapp.com",
+    projectId: "learn-with-firebase-653a1",
+    storageBucket: "learn-with-firebase-653a1.appspot.com",
+    messagingSenderId: "275130694018",
+    appId: "1:275130694018:web:b5266a660e5e244fb0379b"
+  };
 
 // initialze firebase in our project by passing in our environment variables to the `initializeApp` method
 const app = initializeApp(firebaseConfig);
--- utilities/database/StoreToDoItem.js
+++ utilities/database/StoreToDoItem.js
@@ -7,12 +7,21 @@ export async function storeToDoItem(userId, userEmail, itemText) {
         
         /*STEP 5: Get a refernce to the collection we want to store. Lets use the following documentation 
         to figure this out: https://firebase.google.com/docs/firestore/query-data/get-data */
+        const userTodoListCollection = collection(db, `todo-list-${userId}`);
 
 
         // generate a unique id for the document and assemble our data into an object
         const docUniqueId = generateUniqueId();
 
         //STEP 6: Create a object called data that will hold the information going into the document 
+        //Holds all the value going into the user's document when adding a new item to the list
+        //fields:value
+        const data = {
+            userId: userId,
+            email: userEmail,
+            itemText: itemText,
+            uid: docUniqueId,
+        };
 
 
         // store the following data in a document the specified user collection
